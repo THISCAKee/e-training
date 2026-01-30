ef53bd2986916d9959dc8936bb3b8757b4e5e80c
EARTHCAKE <earthcake@EARTHCAKE-10.local>
2026-01-30 16:27:41 +0700

    “”

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="grow">{children}</main>
     </div>
   );
 }
